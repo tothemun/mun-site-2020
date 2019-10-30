69b6fb4962d8c47bf39e7a480b5f368ad1b98871
Jonathan Blair <jon@tothemun.com>
2019-10-30 11:30:47 -0400

    Adding rough support for multiple page types

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,39 +4,67 @@ const path = require('path')
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  return new Promise((resolve, reject) => {
-    const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
-              }
-            }
+  const blogPosts = graphql(`
+    {
+      allContentfulBlogPost {
+        edges {
+          node {
+            title
+            slug
           }
-          `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      }
+    }
+  `).then(results => {
+    if (result.errors) {
+      console.log("Error retrieving contentful data", result.errors);
+    }
 
-        const posts = result.data.allContentfulBlogPost.edges
-        posts.forEach((post, index) => {
-          createPage({
-            path: `/blog/${post.node.slug}/`,
-            component: blogPost,
-            context: {
-              slug: post.node.slug
-            },
-          })
-        })
+    const blogPostTemplate = path.resolve('./src/templates/blog-post.js');
+    const posts = result.data.allContentfulBlogPost.edges;
+
+    posts.forEach((post, index) => {
+      createPage({
+        path: `/blog/${post.node.slug}/`,
+        component: blogPostTemplate,
+        context: {
+          slug: post.node.slug
+        },
       })
-    )
-  })
-}
+    });
+  }).catch(error => {
+    console.log(error);
+  });
+
+  const caseStudies = graphql(`
+    {
+      allContentfulCaseStudy {
+        edges {
+          node {
+            title
+            slug
+          }
+        }
+      }
+    }
+  `).then(result => {
+    console.log("Error retrieving contentful data", result.errors);
+
+    const caseStudyTemplate = path.resolve('./src/templates/caseStudy/caseStudy.js');
+    const caseStudies = result.data.allContentfulCaseStudy.edges;
+
+    caseStudies.forEach((caseStudy, index) => {
+      createPage({
+        path: `work/${caseStudy.node.slug}`,
+        component: caseStudyTemplate,
+        context: {
+          slug: caseStudy.node.slug
+        }
+      })
+    });
+  }).catch(error => {
+    console.log(error);
+  });
+
+  return Promise.all([blogPosts, caseStudies]);
+};
